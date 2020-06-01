3760fa35844a4275f3048fe59086d72f1f0baa8e
Naipunya Naik <43513216+naipunya-naik@users.noreply.github.com>
2020-06-01 13:57:38 +0530

    Create distinctelements_01-06-2020.java

---
+++ JAVA CODING/distinctelements_01-06-2020.java
@@ -0,0 +1,43 @@
+import java.util.*; 
+import java.util.Scanner;  
+public class distinctelements { 
+
+    private static int atMostK(int arr[], int n, int k) 
+    {
+        int left=0,count=0,right=0;
+        HashMap<Integer, Integer> map = new HashMap<>(); 
+         while (right < n) 
+        { 
+            map.put(arr[right], map.getOrDefault(arr[right], 0) + 1); 
+            while (map.size() > k) { 
+                map.put(arr[left], map.get(arr[left]) - 1); 
+                if (map.get(arr[left]) == 0) 
+                    map.remove(arr[left]); 
+                left++; 
+            } 
+            count += right - left + 1; 
+            right++; 
+        } 
+        return count; 
+    } 
+      private static int exactlyK(int arr[], int n, int k) 
+    { 
+        System.out.println("The output is:");  
+        return (atMostK(arr, n, k) - atMostK(arr, n, k - 1)); 
+    } 
+      public static void main(String[] args) 
+    { 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no. of elements you want in the array:");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter all the elements:");
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the integer k:");
+        int k=sc.nextInt();
+          System.out.print(exactlyK(arr, n, k)); 
+    } 
+} 
