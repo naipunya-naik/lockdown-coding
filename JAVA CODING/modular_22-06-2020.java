981181cffcbed1259ea66501c2d0de638a02f3aa
Naipunya Naik <43513216+naipunya-naik@users.noreply.github.com>
2020-06-22 13:06:43 +0530

    Create modular_22-06-2020.java

---
+++ JAVA CODING/modular_22-06-2020.java
@@ -0,0 +1,28 @@
+import java.io.*; 
+import java.util.Scanner;
+public class modular { 
+	static int power(int x, int y, int p) 
+	{ 
+		int res = 1;	 
+		x = x % p; 
+	if (x == 0) return 0;
+    	while (y > 0) 
+		{ 
+			if((y & 1)==1) 
+				res = (res * x) % p; 
+			y = y >> 1; 
+			x = (x * x) % p; 
+		} 
+		return res; 
+	} 
+	public static void main(String args[]) 
+	{ 
+	    int x,y,p;
+	    Scanner scanner= new Scanner(System.in);
+        System.out.print("Enter x,y,p: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+		System.out.println("Power is " + power(a,b,c)); 
+	} 
+} 
